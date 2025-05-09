1ab841fece339cd5b4554dcdb306705ccb1ba6b4
stkeopila <skeopila@calpoly.edu>
2025-05-08 17:35:18 -0700

    added preflop and value to cards

--- PokerBot.py
+++ PokerBot.py
@@ -5,20 +5,39 @@ import random
 
 def initialize_deck():
     suits = ['Diamond', 'Clubs', 'Heart', 'Spade']
-    numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    numbers = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
     deck = []
     for suit in suits:
-        for number in numbers:
-            card = (number, suit)
+        for number, value in numbers.items():
+            card = (number, suit, value)
             deck.append(card)
     return deck
 
 def start_game(deck):
-    cards_out_of_deck = []
-    ran_card = random.choice(deck)
-    cards_out_of_deck.append(ran_card)
-    print(ran_card)
-    print(cards_out_of_deck)
+    removed_cards = []
+    bot, player = pre_flop(deck)
+    for card in bot:
+        removed_cards.append(card)
+    print("Fold or Stay?")
+    answer = input()
+    if answer == "Fold" or "fold":
+        print("Game Over")
+    elif answer == "Stay" or "stay":
+        print("Stay")
+    else:
+        ("Can Only Choose Fold or Stay")
+
+def pre_flop(deck):
+    bot, player = [], []
+    for i in range(2):
+        playerCard = random.choice(deck)
+        player.append(playerCard)
+        deck.remove(playerCard)
+        botCard = random.choice(deck)
+        bot.append(botCard)
+        deck.remove(botCard)
+    print(f"Bot: {bot}\nPlayer: {player}")
+    return(bot, player)
 
 
 if __name__ == "__main__":
