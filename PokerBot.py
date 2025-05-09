0eda8c1b8e608f296f787e0dcb86f0883403b578
stkeopila <skeopila@calpoly.edu>
2025-05-08 18:51:47 -0700

    added flop

--- PokerBot.py
+++ PokerBot.py
@@ -2,6 +2,8 @@ import sys
 import math
 import random
 
+# def hand_value(hand, cards_on_table):
+
 
 def initialize_deck():
     suits = ['Diamond', 'Clubs', 'Heart', 'Spade']
@@ -20,12 +22,23 @@ def start_game(deck):
         removed_cards.append(card)
     print("Fold or Stay?")
     answer = input()
-    if answer == "Fold" or "fold":
+    if answer.lower() == "fold":
         print("Game Over")
-    elif answer == "Stay" or "stay":
-        print("Stay")
+    elif answer.lower() == "stay":
+        cards_on_table, removed_cards = flop(deck, removed_cards)
+        for card, suit, value in cards_on_table:
+            print(f"{card}, {suit}")
     else:
         ("Can Only Choose Fold or Stay")
+
+def flop(deck, removed_cards):
+    cards_on_table = []
+    for i in range(3):
+        flopCard = random.choice(deck)
+        cards_on_table.append(flopCard)
+        deck.remove(flopCard)
+        removed_cards.append(flopCard)
+    return cards_on_table, removed_cards
 
 def pre_flop(deck):
     bot, player = [], []
@@ -36,7 +49,10 @@ def pre_flop(deck):
         botCard = random.choice(deck)
         bot.append(botCard)
         deck.remove(botCard)
-    print(f"Bot: {bot}\nPlayer: {player}")
+    for card, suit, value in bot:
+        print(f"Bot: {card}, {suit}")
+    for card, suit, value in player:
+        print(f"Player: {card}, {suit}")
     return(bot, player)
 
 
